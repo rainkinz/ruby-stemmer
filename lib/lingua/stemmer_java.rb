a28ab03f32392f0051f69e16e45b842661f0ea78
rainkinz <brendan.grainger@gmail.com>
2015-07-28 18:05:01 -0400

    lookup language files

--- lib/lingua/stemmer_java.rb
+++ lib/lingua/stemmer_java.rb
@@ -3,15 +3,32 @@ require 'lingua/ruby-stemmer'
 
 
 module Lingua
+  StemmerError = Class.new(RuntimeError)
+
   class Stemmer
-    def native_init(language, encoding)
-      language = 'english'
+    LANG_LOOKUP = {
+      'en' => 'english',
+      'fr' => 'french',
+      'de' => 'german',
+      'ro' => 'russian'
+    }
+
+    def native_init(lang_code, encoding)
+      language = language_for_code(lang_code)
       @stemmer = org.tartarus.snowball.Stemmer.new
       @stemmer.native_init(language, encoding)
     end
 
     def stem(word)
       @stemmer.stem(word)
+    end
+
+    private
+
+    def language_for_code(lang_code)
+      LANG_LOOKUP.fetch(lang_code) {
+        raise StemmerError, "Could not find implementation for '#{lang_code}'"
+      }
     end
 
   end
